c6ba29fe7e435fd2b04ccca04032b5e66c72d706
GamersVsGaming <jamesromesberg@gmail.com>
2018-05-27 19:06:31 -0400

    Register with username

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const email = document.querySelector("#txtEmail"),
+      username = document.querySelector("#txtUsername"),
       password = document.querySelector("#txtPassword"),
       login = document.querySelector("#btnLogin"),
       signup = document.querySelector("#btnSignUp"),
@@ -17,6 +18,10 @@ signup.addEventListener("click", e =>{
   const auth = firebase.auth();
 
   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  promise.then(u => {
+    user = auth.currentUser;
+    user.updateProfile({displayName: username});
+  });
   promise.catch(e => console.log(e.message));
 });
 
