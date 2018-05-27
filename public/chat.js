faa95d5025e05f589b7e35c97f0bc63290fc9969
GamersVsGaming <jamesromesberg@gmail.com>
2018-05-27 18:54:08 -0400

    fix current user (final)

--- public/chat.js
+++ public/chat.js
@@ -1,39 +1,39 @@
-const port = window.location.hostname;
-//const port = 'localhost:5000';
-const socket = io.connect(port);
+firebase.auth().onAuthStateChanged(user => {
+ if(user){
+   const port = window.location.hostname;
+   //const port = 'localhost:5000';
+   const socket = io.connect(port);
 
-const message = document.getElementById('message'),
-      btn = document.getElementById('send'),
-      output = document.getElementById('output'),
-      feedback = document.getElementById('feedback'),
-      handle = firebase.auth().onAuthStateChanged(user => {
-        if(user){
-          return user.email;
-        }
-      });
+   const message = document.getElementById('message'),
+         btn = document.getElementById('send'),
+         output = document.getElementById('output'),
+         feedback = document.getElementById('feedback'),
+         handle = user.email;
 
-btn.addEventListener('click', () => {
-  socket.emit('chat', {
-    message: message.value,
-    handle: handle
-  });
-  message.value='';
-});
+   btn.addEventListener('click', () => {
+     socket.emit('chat', {
+       message: message.value,
+       handle: handle
+     });
+     message.value='';
+   });
 
-message.addEventListener('keyup', () => {
-  socket.emit('typing', {
-    message: message.value,
-    handle: handle
-  });
-});
+   message.addEventListener('keyup', () => {
+     socket.emit('typing', {
+       message: message.value,
+       handle: handle
+     });
+   });
 
-socket.on('chat', (data) => {
-  feedback.innerHTML = '';
-  output.innerHTML += `<p><strong>${data.handle}:</strong>${data.message}</p>`;
-});
+   socket.on('chat', (data) => {
+     feedback.innerHTML = '';
+     output.innerHTML += `<p><strong>${data.handle}:</strong>${data.message}</p>`;
+   });
 
-socket.on('typing', (data) => {
-  data.message ?
-  feedback.innerHTML = `<p><em>${data.handle} is typing</em></p>` :
-  feedback.innerHTML = '';
+   socket.on('typing', (data) => {
+     data.message ?
+     feedback.innerHTML = `<p><em>${data.handle} is typing</em></p>` :
+     feedback.innerHTML = '';
+   });
+ }
 });
