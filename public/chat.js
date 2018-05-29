22787a94fcc95c832137e0e3241e04a6debc9c44
GamersVsGaming <jamesromesberg@gmail.com>
2018-05-29 15:36:18 -0400

    Disconnect from socket when logout

--- public/chat.js
+++ public/chat.js
@@ -1,9 +1,10 @@
+// Change port for local or live testing
+const port = window.location.hostname;
+//const port = 'localhost:5000';
+const socket = io.connect(port);
+
 firebase.auth().onAuthStateChanged(user => {
  if(user){
-   // Change port for local or live testing
-   const port = window.location.hostname;
-   //const port = 'localhost:5000';
-   const socket = io.connect(port);
 
    const message = document.getElementById('message'),
          btn = document.getElementById('send'),
@@ -61,5 +62,6 @@ firebase.auth().onAuthStateChanged(user => {
    });
  }else{
    document.getElementById('output').innerHTML = '';
+   io.disconnect();
  }
 });
